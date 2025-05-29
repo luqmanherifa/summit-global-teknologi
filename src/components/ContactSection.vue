371780b215360256a8c69439d92a852170ec50c1
Luqman Herifa <luqmanherifa@gmail.com>
2025-05-29 18:26:07 +0700

    Update ContactSection structure and styling

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="contact">
-    <div>
-      <h1>Contact us</h1>
-      <p>Kyiv, Ukraine</p>
-      <p>mail@isa.co.ua</p>
+    <div class="contact__info">
+      <h1 class="contact__title">Contact us</h1>
+      <p class="contact__location">Kyiv, Ukraine</p>
+      <p class="contact__email">mail@isa.co.ua</p>
+      <ul class="contact__social-list">
+        <li
+          class="contact__social-item"
+          v-for="(link, index) in socialLinks"
+          :key="index"
+        >
+          <a
+            class="contact__social-link"
+            :href="link.href"
+            target="_blank"
+            rel="noopener"
+          >
+            {{ link.name }}
+          </a>
+        </li>
+      </ul>
     </div>
-    <ul>
-      <li v-for="(link, index) in socialLinks" :key="index">
-        <a :href="link.href" target="_blank" rel="noopener">{{ link.name }}</a>
-      </li>
-    </ul>
-    <img src="" alt="" />
+    <div>
+      <img
+        class="contact__image"
+        src="../assets/images/contactStarAnimals.svg"
+        alt="Star with animals illustration"
+      />
+    </div>
   </div>
 </template>
 
@@ -37,4 +54,39 @@ export default {
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.contact {
+  display: flex;
+  flex-direction: row;
+  background-color: #31be32;
+  padding: 9rem;
+  text-align: left;
+  color: #000000;
+
+  &__info {
+    flex: 1;
+  }
+
+  &__title {
+    font-size: 3.2rem;
+  }
+
+  &__location,
+  &__email {
+    font-size: 2.2rem;
+  }
+
+  &__social-list {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.5rem;
+    margin-top: 1rem;
+    padding: 0;
+  }
+
+  &__image {
+    width: 540px;
+    height: auto;
+  }
+}
+</style>
