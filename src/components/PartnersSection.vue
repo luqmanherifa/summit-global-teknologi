db8764bd9638886aa908e534ecd0776b668715c6
Luqman Herifa <luqmanherifa@gmail.com>
2025-05-29 18:17:40 +0700

    Update PartnersSection structure and styling

--- src/components/PartnersSection.vue
+++ src/components/PartnersSection.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="partners">
-    <div>
-      <h1>Our partners</h1>
-      <p>are take care of our fund and help us with many questions</p>
+    <div class="partners__header">
+      <h1 class="partners__title">Our partners</h1>
+      <p class="partners__subtitle">
+        are take care of our fund and help us with many questions
+      </p>
     </div>
-    <div>
-      <ul>
-        <li v-for="(partner, index) in partnersList" :key="index">
-          <div>
-            <img src="" alt="" />
+    <div class="partners__list-wrapper">
+      <ul class="partners__list">
+        <li
+          class="partners__item"
+          v-for="(partner, index) in partnersList"
+          :key="index"
+        >
+          <div class="partners__card">
+            <img
+              :src="partner.image"
+              :alt="partner.alt"
+              class="partners__logo"
+            />
           </div>
         </li>
       </ul>
@@ -17,33 +27,79 @@
 </template>
 
 <script>
+import partnersAdviRes from "@/assets/images/partnersAdviRes.jpg";
+import partnersOvidWind from "@/assets/images/partnersOvidWind.png";
+import partnersTbsIndustry from "@/assets/images/partnersTbsIndustry.png";
+import partnersAccurl from "@/assets/images/partnersAccurl.png";
+import partnersLaudisAccountancy from "@/assets/images/partnersLaudisAccountancy.png";
+import partnersKickBite from "@/assets/images/partnersKickBite.jpeg";
+
 export default {
   name: "PartnersSection",
   data() {
     return {
-      citizensList: [
-        {
-          image: "Logo-adviRES",
-        },
-        {
-          image: "ovid-wind-logo",
-        },
-        {
-          image: "TBS Industry",
-        },
-        {
-          image: "Accurl",
-        },
-        {
-          image: "Laudis",
-        },
-        {
-          image: "Kickbite",
-        },
+      partnersList: [
+        { image: partnersAdviRes, alt: "Logo AdviRES" },
+        { image: partnersOvidWind, alt: "Logo Ovid Wind" },
+        { image: partnersTbsIndustry, alt: "Logo TBS Industry" },
+        { image: partnersAccurl, alt: "Logo Accurl" },
+        { image: partnersLaudisAccountancy, alt: "Logo Laudis Accountancy" },
+        { image: partnersKickBite, alt: "Logo Kick Bite" },
       ],
     };
   },
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.partners {
+  display: flex;
+  flex-direction: column;
+  background-color: #ffffff;
+  padding: 9rem;
+  text-align: left;
+  color: #000000;
+  gap: 3rem;
+
+  &__title {
+    font-size: 3.2rem;
+  }
+
+  &__subtitle {
+    font-size: 2.2rem;
+    line-height: 1.5;
+    margin-top: 1rem;
+  }
+
+  &__list-wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+  }
+
+  &__list {
+    padding: 0;
+    gap: 3.2rem;
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  &__item {
+    display: flex;
+    justify-content: center;
+  }
+
+  &__card {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &__logo {
+    width: 227px;
+    height: auto;
+  }
+}
+</style>
